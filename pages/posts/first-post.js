453ee1ba9bec058c962e2d4c4af5911e253743f4
noerios <noerios@gmail.com>
2021-06-03 12:55:25 -0500

    initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+    return (
+      <>
+        <Layout>
+            <Head>
+            <title>First Post</title>
+            </Head>
+            <h1>Kuala Lumpur</h1>
+            <p>
+            The next 365 days have countless experiences, dozens of cities, and at least 10 countries in store. But I can’t really write about what’s coming yet, so I’ll write about what brought me to the spot I’m in now.
+
+A few months ago I started to think about my impending 2-year review at Ayzenberg. What would be my new goals? How could I position myself to do more? What was I going to do to grow creatively and how could I apply this growth to the rest of my life? I did what I usually do when I encounter a huge life dilemma; I picked up my phone and opened Instagram. I stared at photos of people in faraway places and swelled with wanderlust. I quickly scrolled past a sponsored post, as I tend to do, then I backed up — “Travel the world while working remotely. Applications now open.” I was already familiar with Remote Year. A designer at my last agency did this in 2015. This must have been why they were targeting me. Then I thought “Could I do this?” I work as a social media copywriter at an agency that truly seems to care about the professional and personal growth of their employees. I deliver all my work digitally, and do much of my professional collaboration online. But would they go for it? “What the hell, I’ll submit an application. I may not even get a response.” I got a response. The next couple of weeks consisted of a series of secondary applications, Skype interviews, and then an invitation. Now I had to pitch this to my agency. My director cautiously walked into the meeting I had scheduled – “You’re not leaving us are you?”
+
+“Well… Not quite.”
+            </p>
+
+            <h2>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+            </h2>
+        </Layout>
+      </>
+    )
+  }
